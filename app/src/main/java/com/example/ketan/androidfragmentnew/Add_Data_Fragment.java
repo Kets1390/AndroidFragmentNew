23e13ea8927a4c8c9d797ff77dee934a32670520
Ketan Darji <kets1390ketu@gmail.com>
2016-04-06 11:20:51 +0530

    Commit 4

--- app/src/main/java/com/example/ketan/androidfragmentnew/Add_Data_Fragment.java
+++ app/src/main/java/com/example/ketan/androidfragmentnew/Add_Data_Fragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -11,10 +12,23 @@ import android.view.ViewGroup;
  * Created by Ketan on 4/5/2016.
  */
 public class Add_Data_Fragment extends Fragment {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setHasOptionsMenu(true);
+    }
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.add_data_fragment,null,false);
+
         return v;
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.add_action).setVisible(false);
+        super.onPrepareOptionsMenu(menu);
     }
 }
